aa1587450ffabfdbca4d5df08b192e39c48d75aa
Drake <esteban.gobert@me.com>
2018-11-03 17:23:56 +0100

    Base du projet de defi Expertus Application web de type SocialNetwork
    Precisions des definitions de classe et utilisation des BEANs de test

--- src/main/java/com/PublicationTest.java
+++ src/main/java/com/PublicationTest.java
@@ -1,8 +1,12 @@
 package com;
 
+import com.expertus_defi.persistence.StoryRepository;
+import com.expertus_defi.services.FeedService;
+import com.expertus_defi.services.PublicationService;
 import com.expertus_defi.services.Story;
 import com.expertus_defi.web.FeedController;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -10,12 +14,22 @@ import java.util.List;
 
 public class PublicationTest {
 
+    private FeedController feedController;
+
+    @Before
+    public void setUp(){
+        StoryRepository storyRepository = new StoryRepository();
+        PublicationService publicationService = new PublicationService(storyRepository);
+        FeedService feedService = new FeedService(storyRepository);
+        feedController = new FeedController(publicationService, feedService);
+
+    }
+
     @Test
     public void should_post_story() {
 
         //GIVEN
         String story = "hi Expertus !";
-        FeedController feedController = new FeedController();
 
         //WHEN
         feedController.post(story);
--- src/main/java/com/expertus_defi/services/PublicationService.java
+++ src/main/java/com/expertus_defi/services/PublicationService.java
@@ -5,7 +5,11 @@ import com.expertus_defi.persistence.StoryRepository;
 
 public class PublicationService {
 
-    private StoryRepository storyRepository = new StoryRepository();
+    private StoryRepository storyRepository;
+
+    public PublicationService(StoryRepository storyRepository) {
+        this.storyRepository = storyRepository;
+    }
 
     public void share(Story content) {
         storyRepository.save(content);
